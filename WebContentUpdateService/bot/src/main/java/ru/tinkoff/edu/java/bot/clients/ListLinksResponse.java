2aa7092999b94596e37b8f2fb1c24934dfa37770
Julia Kravchenko <js.kravchenko@gmail.com>
2023-05-09 00:00:08 +0200

    HW8 -- added github actions -- test11v4

--- WebContentUpdateService/bot/src/main/java/ru/tinkoff/edu/java/bot/clients/ListLinksResponse.java
+++ WebContentUpdateService/bot/src/main/java/ru/tinkoff/edu/java/bot/clients/ListLinksResponse.java
@@ -1,7 +1,5 @@
 package ru.tinkoff.edu.java.bot.clients;
 
-import ru.tinkoff.edu.java.scrapper.dto.LinkResponse;
-
 import java.util.List;
 
 public record ListLinksResponse(List<LinkResponse> links, Integer size) {
